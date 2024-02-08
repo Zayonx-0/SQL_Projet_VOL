67855e8833bf4bc7217c8e4071752eba061c93db
Zayonx-0 <61589082+Zayonx-0@users.noreply.github.com>
2024-02-08 10:17:01 +0100

    Update derniersreleves.php

--- derniersreleves.php
+++ derniersreleves.php
@@ -19,29 +19,50 @@
   }
 
   // Récupération des informations sur les vols
-  $sql = "SELECT v.callSign, v.codeIATADep, v.codeIATAArr, r.latitude, r.longitude, r.altitude
-    FROM vols v
-    INNER JOIN (
-      SELECT callSign, MAX(heure) AS max_heure
-      FROM releves
-      GROUP BY callSign
-    ) r_max ON v.callSign = r_max.callSign
-    INNER JOIN releves r ON r.callSign = r_max.callSign AND r.heure = r_max.max_heure";
+  $sql = "SELECT v.callSign, v.codeIATADep, v.codeIATAArr, r.latitude, r.longitude, r.altitude,
+  dep.nom AS nomVilleDep, arr.nom AS nomVilleArr
+  FROM vols v
+  INNER JOIN (
+    SELECT callSign, MAX(heure) AS max_heure
+    FROM releves
+    GROUP BY callSign
+  ) r_max ON v.callSign = r_max.callSign
+  INNER JOIN releves r ON r.callSign = r_max.callSign AND r.heure = r_max.max_heure
+  INNER JOIN aeroports dep ON v.codeIATADep = dep.codeIATA
+  INNER JOIN aeroports arr ON v.codeIATAArr = arr.codeIATA;";
+
   $result = $conn->query($sql);
 
     // Affichage des informations pour le dernier vol
     while ($row = $result->fetch_assoc()) {
   
       echo "<h2>Vol " . $row["callSign"] . "</h2>";
-      echo "<p>Départ: " . $row["codeIATADep"] . "</p>";
-      echo "<p>Arrivée: " . $row["codeIATAArr"] . "</p>";
+      echo "<p>Départ: " . $row["nomVilleDep"] . "</p>";
+      echo "<p>Arrivée: " . $row["nomVilleArr"] . "</p>";
       echo "<p>Dernier relevé:</p>";
       echo "<ul>";
       echo "<li>Latitude: " . $row["latitude"] . "</li>";
       echo "<li>Longitude: " . $row["longitude"] . "</li>";
       echo "<li>Altitude: " . $row["altitude"] . "</li>";
+      echo "<br>";
+      echo "</ul>";
+
+          // Requête pour récupérer l'historique des relevés pour le callSign actuel
+    $historiqueSql = "SELECT latitude, longitude, altitude, heure FROM releves WHERE callSign = '" . $row["callSign"] . "'";
+    $historiqueResult = $conn->query($historiqueSql);
+
+    echo "Historique des relevés pour ce vol : <br>";
+    // Affichage de l'historique des relevés
+    while ($historiqueRow = $historiqueResult->fetch_assoc()) {
+      echo "<li>Heure: " . $historiqueRow["heure"] . "</li>";
+      echo "<li>Latitude: " . $historiqueRow["latitude"] . "</li>";
+      echo "<li>Longitude: " . $historiqueRow["longitude"] . "</li>";
+      echo "<li>Altitude: " . $historiqueRow["altitude"] . "</li>";
+      echo "<br>";
+      
       echo "</ul>";
     }
+  }
   // Fermeture de la connexion à la base de données
   $conn->close();
   ?>
